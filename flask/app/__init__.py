073f9844f33bbdfc774d60d934da6b7b107c170a
nnyPRO <nanmesanee.17@gmail.com>
2024-02-06 12:49:23 +0700

    db connection

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__, static_folder='static')
@@ -10,10 +12,17 @@ app.jinja_options.update({
     'lstrip_blocks': True
 })
 
-app.config['JSON_AS_ASCII'] = False
+
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = \
     '5f0be0bc90b9160b99991d46fbf7f3e924696932925d043c'
+app.config['JSON_AS_ASCII'] = False
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite://")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Creating an SQLAlchemy instance
+db = SQLAlchemy(app)
 
 from app import views # noqa
 
--- flask/app/views.py
+++ flask/app/views.py
@@ -1,12 +1,24 @@
 import json
 from flask import (jsonify, render_template,
                    request, url_for, flash, redirect)
+from sqlalchemy.sql import text
 from app import app
+from app import db
 
 
 @app.route('/')
 def home():
     return "Flask says 'Hello world!'"
+
+
+@app.route('/db')
+def db_connection():
+    try:
+        with db.engine.connect() as conn:
+            conn.execute(text("SELECT 1"))
+        return '<h1>db works.</h1>'
+    except Exception as e:
+        return '<h1>db is broken.</h1>' + str(e)
 
 
 @app.route('/lab04')
